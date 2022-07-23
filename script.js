c012cc0ac6e9dea4c85a3677163f68a2efc66aec
Glitch (glitch-hello-website) <none>
2022-07-23 21:01:18 +0000

    🎰🌞 Checkpoint ./script.js:646160/299 ./index.html:646160/134 ./style.css:646160/600

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const birds =  [
     "name": "Common Loon",
     "url" : "https://cdn.glitch.global/457b3231-6ea2-4da0-a5e5-e4f8765ce99d/XC432766%20-%20Common%20Loon%20-%20Gavia%20immer.mp3?v=1658608941259",
     "copyright" : "Stanislas Wroza",
-    "xeno-link": "https://xeno-canto.org/432766",
-    "code": "1"
+    "xenolink": "https://xeno-canto.org/432766"
   }
 ]
 
@@ -18,7 +17,7 @@ var sound = new Howl({
   html5: true,
   onend: function() {
     $("#play").removeClass("disable")
-    $("#play").html("Play ");
+    $("#play").html("Play bird sound");
   }
 });
 
@@ -41,3 +40,4 @@ sound.play();
   $("#play").html("Playing sound...");
   });
 
+$(".credit").html("Sound recorded by " + "<a href='"+birds[0].xenolink+"'>"+birds[0].copyright+"</a>")
