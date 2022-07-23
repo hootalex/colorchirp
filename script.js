0749bd82e9f602be73aad04ee769aa58c6c43c4d
Glitch (glitch-hello-website) <none>
2022-07-23 19:40:15 +0000

    📊🚓 Checkpoint ./script.js:646160/999 ./index.html:646160/147 ./style.css:646160/312

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+var maincolor = "transparent"
+
 $( ".color" ).on( "click", function( event ) {
   $(".color").removeClass("selected")
-  if ($(this).hasClass("selected")) { 
-     $(".color").removeClass("selected")
-  } else {
   $(this).addClass("selected")
+  maincolor = $(this).css("background-color")
+  $(".shape>svg").css("fill", maincolor)
+  
+  if (maincolor == "rgb(255, 255, 255)") {
+    $(".shape").css("background-color","#9e9696")
+  } else {
+    $(".shape").css("background-color","transparent")
   }
+});
+
+$( ".shape" ).on( "click", function( event ) {
+  $(".shape").removeClass("selected")
+  $(this).addClass("selected")
 });
