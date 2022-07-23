e7842cb2c9cf5c6c0d7bb2d1788b9bd0f7b22699
Glitch (glitch-hello-website) <none>
2022-07-23 19:52:14 +0000

    🌵🌸 Checkpoint ./index.html:646160/248 ./birds.json:646160/18 ./birds.js:646160/4510

---
+++ birds.js
@@ -0,0 +1,7 @@
+const birds =  [
+   {
+    "name": "Northern Cardinal",
+    "url" : "https://nas-national-prod.s3.amazonaws.com/birds/audio/NORCAR_1.songsnum1_NYle_1.mp3",
+    "copyright" : "Audio © Lang Elliott, Bob McGuire, Kevin Colver, Martyn Stewart and others."
+  }
+]
