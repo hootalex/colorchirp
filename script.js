6d6f178b0eb2b854dbda682a82a22ff7dcdb4440
Glitch (glitch-hello-website) <none>
2022-07-23 20:16:13 +0000

    🐺🚨 Checkpoint ./style.css:646160/107 ./index.html:646160/19 ./script.js:646160/162

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ $( ".color" ).on( "click", function( event ) {
   maincolor = $(this).css("background-color")
   $(".shape>svg").css("fill", maincolor)
   
-  if (maincolor == "rgb(255, 255, 255)") {
-    $(".shape").css("background-color","#9e9696")
-  } else {
-    $(".shape").css("background-color","transparent")
-  }
 });
 
 $( ".shape" ).on( "click", function( event ) {
