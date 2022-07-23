a1c899db254973eaabcf0a8e22b5fe5283284b83
Glitch (glitch-hello-website) <none>
2022-07-23 19:28:17 +0000

    🌂🙇 Checkpoint ./style.css:646160/476 ./index.html:646160/171 ./script.js:646160/519

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hi")
+$( ".color" ).on( "click", function( event ) {
+  $(".color").removeClass("selected")
+  if ($(this).hasClass("selected")) { 
+     $(".color").removeClass("selected")
+  } else {
+  $(this).addClass("selected")
+  }
+});
