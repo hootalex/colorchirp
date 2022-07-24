051fc8ffa54f2fdbf6b84ad9506b64050df75bf5
Glitch (glitch-hello-website) <none>
2022-07-24 20:28:19 +0000

    🐏🏃 Checkpoint ./script.js:646160/36 ./style.css:646160/419

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var maxval = 88
 function drawchart(){
 
 jQuery.each(chartdata, function(index, value) {
-  $('.chart').append("<p>"+value[0]+"<sub> "+value[1]+unit+ "</sub></p><div class='bar' style='width:"+value[1].replace(/\D/g,'') / maxval +"%;background-color:"+value[2]+"'></div>")
+  $('.chart').append("<p>"+value[0]+"<sub> "+value[1]+unit+ "</sub></p><div class='bar' style='width:"+value[1] / maxval*100 +"%;background-color:"+value[2]+"'></div>")
 });
 
 }
